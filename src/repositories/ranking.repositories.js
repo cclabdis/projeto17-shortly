07a8e4a5ccfedcd803694110464f580bff91ee99
cclabdis <cclabdis@gmail.com>
2023-08-07 17:57:54 -0300

    modified repositoriesr

--- src/repositories/ranking.repositories.js
+++ src/repositories/ranking.repositories.js
@@ -5,14 +5,14 @@ export  async function rankingRepository(){
     return db.query(`
       SELECT
           users.user_id AS id,
-          users.name,
-          COUNT(urls.url_id) AS "linksCount",
-          SUM(urls.visit_count) AS "visitCount"
+          users.name AS name,
+          COUNT(links.url_id) AS "linksCount",
+          SUM(links.visit_count) AS "visitCount"
       FROM
           users
-          LEFT JOIN urls ON users.user_id = urls.user_id
+          LEFT JOIN links ON users.user_id = links.user_id
       GROUP BY
-          users.user_id, users.name, urls.visit_count
+          users.user_id, users.name, links.visit_count
       ORDER BY
           "visitCount" DESC
       LIMIT
